3353e9586f354311bb54ba348d967b714d707d5e
Boderio <berti@epse.be>
2025-12-10 11:02:33 +0100

    correction exercice Routing

---
+++ app/reviews/spider-man/page.jsx
@@ -0,0 +1,8 @@
+export default function SpiderManPage() {
+    return (
+        <>
+            <h1>SpiderMan</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit sequi modi dolorem aliquam nam voluptatibus magni. Laudantium velit vero ut placeat aliquid non minima rerum tempora numquam quae nemo, perferendis officiis qui facere, temporibus repudiandae vitae quaerat reiciendis architecto. Autem, minus molestias exercitationem iusto maxime quo ullam, fugiat quam corporis quaerat animi at voluptatem expedita? Blanditiis officiis illum nisi odio beatae similique! Nam in molestias, perferendis omnis eum inventore ad ratione similique temporibus culpa voluptatibus alias exercitationem quos! Asperiores eos ex qui dignissimos quo, nobis doloribus quidem provident. Possimus illum dignissimos, aut earum consequatur aliquid deserunt tenetur perferendis repellendus alias!</p>
+        </>
+    )
+}
