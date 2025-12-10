d381bdfc05e817513b392d0faa1d2476a2fa2cb7
Boderio <berti@epse.be>
2025-12-10 10:48:11 +0100

    routing Router App

---
+++ app/reviews/diablo/page.jsx
@@ -0,0 +1,8 @@
+export default function DiabloPage(){
+    return (
+        <>
+            <h1>Diablo IV</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus aut explicabo optio nulla ut esse magni tempora temporibus vel officia consequatur natus tempore accusamus sit in facilis laboriosam repellat iste, eum odio corporis quod dolore autem. Saepe iusto iste pariatur, doloremque vel praesentium. Repellat nobis aliquam aliquid dicta possimus in et corporis error. Mollitia ab excepturi cupiditate maiores. Consequuntur, natus!</p>
+        </>
+    )
+}
