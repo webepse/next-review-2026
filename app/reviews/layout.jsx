21111993367a8aeb2a7669db7eb69e896c37befc
Boderio <berti@epse.be>
2025-12-10 11:11:33 +0100

    Nested Layout

---
+++ app/reviews/layout.jsx
@@ -0,0 +1,11 @@
+export default function ReviewsLayout({children})
+{
+    return (
+        <div>
+            <div>
+                barre de menu spécifique à review
+            </div>
+            {children}
+        </div>
+    )
+}
