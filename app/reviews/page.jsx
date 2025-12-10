9a9f0f14403654722300b33a8117a9effe0c2e92
Boderio <berti@epse.be>
2025-12-10 11:35:23 +0100

    link dans page.jsx de reviews

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -1,8 +1,14 @@
+import Link from "next/link"
+
 export default function ReviewsPage(){
     return (
         <>
             <h1>Reviews</h1>
             <p>Here we'll list all the reviews</p>
+            <ul>
+                <li><Link href='/reviews/diablo'>Diablo IV</Link></li>
+                <li><Link href='/reviews/spider-man'>Spider-man</Link></li>
+            </ul>
         </>
     )
 }
