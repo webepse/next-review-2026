967f92ee4f393a89a93dece8326c5d33d8fc83ed
Boderio <berti@epse.be>
2025-12-10 11:28:08 +0100

    prefetch + del reviews/layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export default function RootLayout({children})
                         <ul>
                             <li><Link href="/">Home</Link></li>
                             <li><Link href="/reviews">Reviews</Link></li>
-                            <li><Link href="/about">About</Link></li>
+                            <li><Link href="/about" prefetch={false}>About</Link></li>
                         </ul>
                     </nav>
                 </header>
